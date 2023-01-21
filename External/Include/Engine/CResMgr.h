a019924313222a6f14279a283449a5cb3e3f0b95
건효 정 <rjsgy08@gmail.com>
2023-01-21 23:47:02 +0900

    정건효 : ComputeShader와 기존 방식 비교 1. Texture Create 함수 추가 2. CDevice RenderTarget, DepthStencil 자체 CTexture 타입으로 변환 3. 생성시킨 텍스쳐를 RenderTarget 으로 지정, 원하는 색상을 칠함 4. 색상을 변경시킨 텍스쳐를 다시 몬스터 오브젝트를 통해서 화면에 출력해봄

--- External/Include/Engine/CResMgr.h
+++ External/Include/Engine/CResMgr.h
@@ -5,6 +5,7 @@
 #include "CMesh.h"
 #include "CTexture.h"
 #include "CGraphicsShader.h"
+#include "CComputeShader.h"
 #include "CMaterial.h"
 #include "CPrefab.h"
 
@@ -24,12 +25,19 @@ public:
 private:
     void CreateDefaultMesh();
     void CreateDefaultGraphicsShader();
+    void CreateDefaultComputeShader();
     void CreateDefaultMaterial();
     void CreateDefaultPrefab();
     void LoadDefaultTexture();
 
 public:
     const map<wstring, Ptr<CRes>>& GetResources(RES_TYPE _Type) { return m_arrRes[(UINT)_Type]; }
+
+    // _BindFlag = D3D11_BIND_FLAG
+    Ptr<CTexture> CreateTexture(const wstring& _strKey, UINT _Width, UINT _Height
+        , DXGI_FORMAT _pixelformat, UINT _BindFlag, D3D11_USAGE _Usage);
+
+    Ptr<CTexture> CreateTexture(const wstring& _strKey, ComPtr<ID3D11Texture2D> _Tex2D);
 
 
     template<typename T>
@@ -40,6 +48,8 @@ public:
 
     template<typename T>
     Ptr<T> Load(const wstring& _strKey, const wstring& _strRelativePath);
+
+
 };
 
 template<typename T>
@@ -52,12 +62,14 @@ RES_TYPE GetResType()
     //const type_info& sound = typeid(CSound);
     const type_info& prefab = typeid(CPrefab);
     const type_info& gs = typeid(CGraphicsShader);
-    //const type_info& cs = typeid(CComputeShader);
+    const type_info& cs = typeid(CComputeShader);
 
     if (typeid(T).hash_code() == mesh.hash_code())
         return RES_TYPE::MESH;
     if (typeid(T).hash_code() == gs.hash_code())
         return RES_TYPE::GRAPHICS_SHADER;
+    if (typeid(T).hash_code() == cs.hash_code())
+        return RES_TYPE::COMPUTE_SHADER;
     if (typeid(T).hash_code() == texture.hash_code())
         return RES_TYPE::TEXTURE;
     if (typeid(T).hash_code() == material.hash_code())
